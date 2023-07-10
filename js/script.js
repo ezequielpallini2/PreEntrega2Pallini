fdfe8a73d65bd305528f7494e7486732b4d19201
EZEQUIEL PALLINI <ezequielpallini2@gmail.com>
2023-07-09 22:01:25 -0300

    actualizaciónes

--- js/script.js
+++ js/script.js
@@ -1,61 +1,78 @@
-var cant_entradas = parseFloat(prompt("Ingrese la cantidad de entradas que desea comprar"));
+var cant_entradas = parseFloat(prompt("Ingrese el número de show que desea comprar"));
 var precio_total = 0;
 var precio_entrada = 0;
 
-get_entradas();
 
-function get_entradas() {
-  for (let i = 1; i <= cant_entradas; i++) {
-    precio_entrada = entrada_elegido(i);
-    precio_total = precio_total + precio_entrada;
-    alert("El precio de la entrada número " + i + " es de " + precio_entrada + " y el precio total de su compra es de " + precio_total + ". Le faltan " + (cant_entradas - i) + " entradas");
+class Evento {
+  constructor(id, titulo, locacion, capacidad) {
+    this.id = id;
+    this.titulo = titulo;
+    this.locacion = locacion;
+    this.capacidad = capacidad;
   }
 
-  console.log("El precio total de la compra actualmente es de " + precio_total);
-
-  if (precio_total > 5000) {
-    alert("¡Su compra supera los 5000 pesos! Deberá utilizar otra tarjeta de crédito para seguir comprando");
+  
+  // Getter
+  getTitulo() {
+    return this.titulo;
   }
 
-  alert("El precio total de su compra final es de " + precio_total);
-  console.log("El precio total de la compra final es de " + precio_total);
-}
+  // Setter
+  setTitulo(nuevoTitulo) {
+    this.titulo = nuevoTitulo;
+  }
 
-function entrada_elegido(i) {
-  let continuar = false;
-  let precio_entrada = 0;
+  // Getter
+  getId() {
+    return this.id;
+  }
 
-  do {
-    let elegir_entrada = prompt("Ingrese el número del show a comprar, del 1 al 6.");
+  // Setter
+  setId(nuevaId) {
+    this.id = nuevaId;
+  }
 
-    if (elegir_entrada <= 6 && elegir_entrada >= 1) {
-      alert("Eligió la entrada " + elegir_entrada);
-      precio_entrada = obtener_precio_entrada(elegir_entrada, i);
-      continuar = true;
-    } else {
-      alert("No existe la entrada " + elegir_entrada);
-    }
-  } while (continuar !== true);
-
-  return precio_entrada;
-}
-
-function obtener_precio_entrada(elegir_entrada, i) {
-  const precios_entradas = {
-    "1": 3500,
-    "2": 2200,
-    "3": 2000,
-    "4": 4000,
-    "5": 1000,
-    "6": 1500
-  };
-
-  if (precios_entradas.hasOwnProperty(elegir_entrada)) {
-    const precio = precios_entradas[elegir_entrada];
-    console.log(`El precio del show ${i} es de $ ${precio}`);
-    return precio;
-  } else {
-    console.warn("No eligió un show válido");
-    return null;
+  // Método toString
+  toString() {
+    return `Titulo:  ------------------------------- ${this.titulo}, Id:  ------------------------------- ${this.id}`;
   }
 }
+
+// Crear una instancia de la clase Evento
+const evento = new Evento("Agustín canapino hara trompos en el obelisco", 98);
+
+// Acceder a los métodos y propiides
+console.log(evento.getTitulo()); 
+console.log(evento.getId()); 
+
+evento.setTitulo("Osama Bin Laden tocará gratis en la casa blanca");
+evento.setId(911);
+
+const eventos = [];
+
+// Agregar eventos al array
+eventos.push(new Evento(325, "evento 1",  "Movistar Arena", "90000"));
+eventos.push(new Evento(3011, "evento 2",  "Movistar Arena", "90000"));
+eventos.push(new Evento(232,  "evento 3", "Movistar Arena", "90000"));
+
+
+
+
+
+// Obtengo todos los elementos <h2> y <p> dentro de los elementos con la clase "text" en mi página, serían los títulos de los shows
+const elements = document.querySelectorAll('.text h2, .text p');
+
+let id = 1;
+// Recorro los elementos y extraigo la información
+elements.forEach(element => {
+  const titulo = element.textContent;
+  eventos.push(new Evento(id, titulo, "default", 9999));
+  id++;
+});
+
+console.log(eventos);
+
+inputSearch.addEventListener('search', ()=> {
+  const resultado = eventos.filter((titulo)=> prenda.titulo.toLowerCase().includes(inputSearch.value.toLowerCase()))
+  cargarShows(resultado)
+})
