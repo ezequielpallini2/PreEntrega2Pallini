461846bfa32bec5ec8de890204f15d4768571d9d
Ezequiel Pallini <ezequielpallini2@gmail.com>
2023-07-26 10:21:58 -0300

    Ahora funciona el checkout

--- js/checkout.js
+++ js/checkout.js
@@ -1,35 +1,50 @@
-    const tableBody = document.querySelector('tbody');
-    const cantidadSeleccionada = document.querySelector("#numeroSeleccionado") || 1;
+const tableBody = document.querySelector('tbody');
+const cantidadSeleccionada = document.querySelector("#numeroSeleccionado") || 1;
+const eventoSelect = eventoSeleccionado;
+const precioEvento = eventoSeleccionado.price;
 
-    function mostrarEventoHTML(evento, cantidad) {
-        return `<tr>
-                    <td>
-                        <div class="div-card" style="background-image: url('${evento.imagen}'); background-size: cover;  background-color: rgba(255, 255, 255, 0.1);">
-                    </td>
-                    <td>
-                        <tr>
-                            ${evento.description}
-                        </tr>
-                        <tr>
-                            Cantidad de entradas:
-                            <select id="numeroSeleccionado">
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                            </select>
-                        </tr>
-                        <tr>
-                            Total:${evento.price * cantidad}
-                        </tr>
-                        <tr>
-                            <div class="centrar-div">
-                                <button class="button" id="btnComprar">COMPRAR</button>
-                            </div>
-                        </tr>
-                    </td>
-                </tr>`;
-    }
+function actualizarTotal(cantidad) {
+    const total = precioEvento * cantidad;
+    const totalElement = document.querySelector("#total");
+    totalElement.textContent = `Total: ${total}`;
+}
 
-        tableBody.innerHTML = mostrarEventoHTML(eventoSeleccionado, cantidadSeleccionada);
+
+function mostrarEventoHTML(evento, cantidad) {
+    return `<tr>
+                <td>
+                    <div class="div-card" style="background-image: url('${evento.imagen}'); background-size: cover;  background-color: rgba(255, 255, 255, 0.1);">
+                    </div>
+                </td>
+                <td>
+                    <tr>
+                        ${evento.description}
+                    </tr>
+                    <tr>
+                        Cantidad de entradas:
+                        <select id="numeroSeleccionado">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                    </tr>
+                    <tr id="totalRow">
+                        Total:${evento.price * cantidad}
+                    </tr>
+                    <tr>
+                        <div class="centrar-div">
+                            <button class="button" id="btnComprar">COMPRAR</button>
+                        </div>
+                    </tr>
+                </td>
+            </tr>`;
+}
+
+tableBody.innerHTML = mostrarEventoHTML(eventoSelect, cantidadSeleccionada.value);
+
+cantidadSeleccionada.addEventListener("change", function () {
+    const cantidad = parseInt(cantidadSeleccionada.value);
+    actualizarTotal(cantidad);
+});
--- js/index.js
+++ js/index.js
@@ -11,23 +11,29 @@ function retornarCardError() {
 }
 
 
-function retornarCardHTML({ id, imagen, description, price } = evento ) {
-   return `<div class="div-card" style="background-image: url('${imagen}'); background-size: cover;  background-color: rgba(255, 255, 255, 0.1);">
+function retornarCardHTML({ id, imagen, description, price } = evento) {
+    return `<div class="div-card" style="background-image: url('${imagen}'); background-size: cover;  background-color: rgba(255, 255, 255, 0.1);">
                 <div class="evento">
                     <p>${description}</p> 
                 </div>
                 <div class="prenda">
                     <p>$ ${price}</p>
                 </div>
-                <a href="checkout.html"><button class="button button-outline button-add" id="${id}" >comprar</button></a>
+                <a href="checkout.html" ><button class="button button-outline button-add" id="${id}" >comprar</button></a>
            </div>`
 }
 function activarClickEnBotones() {
     const botones = document.querySelectorAll('button.button.button-outline.button-add');
     botones.forEach((boton) => {
+        console.log(boton);
         boton.addEventListener('click', () => {
-            let evento = arrayeventos.find((evento) => evento.id === parseInt(evento.id));
+            console.log(boton.id);
+
+            let evento = arrayeventos.find((evento) => parseInt(evento.id) === parseInt(boton.id));
+            
+            console.log(evento.id);
             localStorage.setItem('eventoSeleccionado', JSON.stringify(evento));
+            console.log(evento);
         });
     });
 }
@@ -36,15 +42,15 @@ function activarClickEnBotones() {
 
 function cargareventos(array) {
     container.innerHTML = ""
-    array.forEach((evento)=> container.innerHTML += retornarCardHTML(evento) )
-    activarClickEnBotones()
+    array.forEach((evento) => container.innerHTML += retornarCardHTML(evento))
+    activarClickEnBotones();
 }
 
 //función principal que cargar (o no) los eventos en el HTML
 arrayeventos.length > 0 ? cargareventos(arrayeventos) : container.innerHTML = retornarCardError()
 
-inputSearch.addEventListener('search', ()=> {
+inputSearch.addEventListener('search', () => {
     localStorage.setItem("ultimaBusqueda", inputSearch.value)
-    const resultado = arrayeventos.filter((evento)=> evento.description.toLowerCase().includes(inputSearch.value.toLowerCase()))
+    const resultado = arrayeventos.filter((evento) => evento.description.toLowerCase().includes(inputSearch.value.toLowerCase()))
     cargareventos(resultado)
 })
